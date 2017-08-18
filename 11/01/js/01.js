c4438ffa6bd8f6dbc9b951f6e754711261a595a8
jiangheyan <jiangheyan@9hds.com>
2017-08-18 14:57:04 +0800

    查找与替换模拟

--- 11/01/js/01.js
+++ 11/01/js/01.js
@@ -1,26 +1,107 @@
-var searchTxt = document.getElementById('searchTxt');
-var changeTxt = document.getElementById('changeTxt');
+var open = document.getElementById('open');
+var nav = document.getElementById('nav');
+var bottom = document.getElementById('bottom');
+var tab = document.getElementById('tab');
+var txt0 = document.getElementById('txt0');
+var txt1 = document.getElementById('txt1');
+var txt2 = document.getElementById('txt2');
+var look = document.getElementById('look');
 var change = document.getElementById('change');
-var con = document.getElementById('con');
-
+var close = document.getElementById('close');
+var win = document.getElementById('win');
+var container = document.getElementById('container');
+var aP = container.getElementsByTagName('p')[0];
+var tabList = tab.getElementsByTagName('li');
+var openUl = nav.getElementsByTagName('ul')[0];
+var openList = openUl.getElementsByTagName('li');
+var winDiv = bottom.getElementsByTagName('div');
+var onOff = true;
+var on = true;
+open.onclick = function(e) {
+    win.style.display = 'block';
+    if (on) {
+        openUl.style.display = 'block';
+    } else {
+        openUl.style.display = 'none';
+    }
+    on = !on;
+    // e.stopPropagation();
+};
+var oldtxt = aP.innerHTML;
+look.onclick = function() {
+    var txt = txt0.value;
+    if (!txt) {
+        alert('请输入查找关键字');
+    } else {
+        aP.innerHTML = oldtxt.split(txt).join('<span class="hightLight">' + txt + '</span>');
+    }
+};
 change.onclick = function() {
-    var sTxt = searchTxt.value;
-    var cTxt = changeTxt.value;
-    var conTxt = con.innerHTML;
-    if (!sTxt) return;
-    var newTxt = conTxt.split(sTxt).join('<span class="hightLight">' + cTxt + '</span>');
-    con.innerHTML = newTxt;
+    var txtone = txt1.value;
+    var txttwo = txt2.value;
+    if (!txtone) {
+        alert('请输入查找关键字');
+        return;
+    } else {
+        aP.innerHTML = oldtxt.split(txtone).join(txttwo);
+    }
+};
+document.onclick = function(ev) {
+    var event = ev || window.event;
+    var target = event.target || event.srcElement;
+    while (target) {
+        if (target.id && target.id.toLowerCase() == 'open') {
+            return false;
+        }
+        // target = target.parentNode; //层层遍历上去，直到target = null; 结束while循环
+        target = 0; //直接令target = 0; 结束循环
+    }
+    openUl.style.display = 'none';
+};
+close.onclick = function() {
+    win.style.display = 'none';
 };
 
-$(document).bind('click', function(e) {
-    var e = e || window.event; //浏览器兼容性
-    var elem = e.target || e.srcElement;
-    while (elem) { //循环判断至跟节点，防止点击的是div子元素
-        if (elem.id && elem.id == 'test') {
-            return;
-        }
-        elem = elem.parentNode;
-    }
+for (var i = 0; i < openList.length; i++) {
+    openList[i].index = i;
+    openList[i].onclick = function() {
+        hide(winDiv);
+        isAvtive(tabList);
+        isAvtive(openList);
+        winDiv[this.index].style.display = 'block';
+        tabList[this.index].className = 'active_nav';
+        this.className = 'active_nav';
+    };
+}
+for (var j = 0; j < tabList.length; j++) {
+    tabList[j].index = j;
+    tabList[j].onclick = function() {
+        hide(winDiv);
+        isAvtive(tabList);
+        isAvtive(openList);
+        winDiv[this.index].style.display = 'block';
+        openList[this.index].className = 'active_nav';
+        this.className = 'active_nav';
+    };
+}
 
-    $('#test').css('display', 'none'); //点击的不是div或其子元素
-});
+function hide(obj) {
+    for (var i = 0; i < obj.length; i++) {
+        obj[i].style.display = 'none';
+    }
+}
+
+function isAvtive(obj) {
+    for (var i = 0; i < obj.length; i++) {
+        obj[i].className = '';
+    }
+}
+
+function show(obj) {
+    if (onOff) {
+        obj.style.display = 'block';
+    } else {
+        obj.style.display = 'none';
+    }
+    onOff = !onOff;
+}
